0bfa923ea31aa0c8142eb9c9939d69c18688d18a
Andrew Vy <andrew@andrewvy.com>
2018-12-17 09:15:46 -0800

    Handle wrapping errors from platform notifiers

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 const os = require('os').platform();
-const loadE = moduleName => require(moduleName).on('error', () => {});
 
-module.exports = (() => {
+const loadPlatformNotifier = () => {
   switch (os) {
-    case 'darwin': return loadE('./macos');
-    case 'linux': return loadE('./linux');
-    case 'win32': return loadE('./windows');
+    case 'darwin': return require('./macos');
+    case 'linux': return require('./linux');
+    case 'win32': return require('./windows');
+    default: return () => {};
   }
+}
 
-  return () => {};
-})();
+module.exports = (opts) => {
+  const platformNotifier = loadPlatformNotifier()
+
+  return platformNotifier(opts).on('error', () => {})
+}
